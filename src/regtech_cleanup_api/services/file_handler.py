1af690a27daa8671243783398c8441f7567cc2dc
jmcguffee <justin.mcguffee@cfpb.gov>
2024-08-14 13:39:18 -0400

    Added delete from s3 bucket function

---
+++ src/regtech_cleanup_api/services/file_handler.py
@@ -0,0 +1,28 @@
+import shutil
+from http import HTTPStatus
+from pathlib import Path
+from typing import Any
+
+import boto3
+from regtech_api_commons.api.exceptions import RegTechHttpException
+
+from regtech_cleanup_api.config import filing_settings, FsProtocol
+
+
+def delete(path: str):
+    if filing_settings.fs_upload_config.protocol == FsProtocol.FILE:
+        shutil.rmtree(f"{filing_settings.fs_upload_config.root}/{path}")
+    else:
+        bucket = filing_settings.fs_upload_config.root
+        s3 = boto3.resource("s3")
+        s3_objects = s3.meta.client.list_objects(Bucket=bucket, Prefix=path)
+        if "Contents" in s3_objects:
+            object_keys = {"Objects": [{"Key": k["Key"]} for k in s3_objects["Contents"]]}
+            s3.meta.client.delete_objects(Bucket=bucket, Delete=object_keys)
+        else:
+            raise RegTechHttpException(
+                status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                name="Content Not Available",
+                detail="Associated LEI data is not present for removal",
+            )
+
