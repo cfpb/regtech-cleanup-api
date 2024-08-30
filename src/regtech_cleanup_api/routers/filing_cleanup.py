dbd3b9cac59654ca52785bab222a25ec23ac1a8c
jmcguffee <justin.mcguffee@cfpb.gov>
2024-08-30 15:57:22 -0400

    Added many db calls and finished delete filing endpoint

--- src/regtech_cleanup_api/routers/filing_cleanup.py
+++ src/regtech_cleanup_api/routers/filing_cleanup.py
@@ -14,6 +14,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from regtech_api_commons.api.dependencies import verify_user_lei_relation
 
+import regtech_cleanup_api.entities.repos.filing_repo as repo
 from regtech_cleanup_api.services.cleanup_processor import delete_from_storage
 from regtech_cleanup_api.services.validation import is_valid_cleanup_lei
 
@@ -30,10 +31,62 @@ executor = ProcessPoolExecutor()
 router = Router(dependencies=[Depends(set_db), Depends(verify_user_lei_relation)])
 
 
-@router.get("/institutions/{lei}/filings/{period_code}")
-def delete_filing(request: Request, lei: str, period_code: str):
+@router.delete("/institutions/{lei}/filings/{period_code}")
+async def delete_filing(request: Request, lei: str, period_code: str):
     if is_valid_cleanup_lei(lei):
-        delete_from_storage(period_code, lei)
+        try:
+            session = request.state.db_session
+            try:
+                await repo.delete_contact_info(session, lei, period_code)
+            except Exception as e:
+                raise RegTechHttpException(
+                    status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                    name="S3 Bucket Delete Failed",
+                    detail="Failed to delete data from s3",
+                ) from e
+
+            try:
+                user_action_ids = await repo.get_user_action_ids(
+                    session, lei, period_code
+                )
+            except Exception as e:
+                raise RegTechHttpException(
+                    status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                    name="Missing User Action Data",
+                    detail="Failed to get user action data",
+                ) from e
+
+            try:
+                await repo.delete_submissions(session, lei, period_code)
+            except Exception as e:
+                raise RegTechHttpException(
+                    status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                    name="Submission Delete Failed",
+                    detail="Failed to delete submission data",
+                ) from e
+
+            try:
+                await repo.delete_filing(session, lei, period_code)
+            except Exception as e:
+                raise RegTechHttpException(
+                    status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                    name="Filing Delete Failed",
+                    detail="Failed to delete filing data",
+                ) from e
+
+            try:
+                await repo.delete_user_actions(session, user_action_ids)
+            except Exception as e:
+                raise RegTechHttpException(
+                    status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                    name="User Action Delete Failed",
+                    detail="Failed to delete user action data",
+                ) from e
+
+            delete_from_storage(period_code, lei)
+
+        except Exception as e:
+            raise e
     else:
         raise RegTechHttpException(
             status_code=HTTPStatus.NOT_ACCEPTABLE,
