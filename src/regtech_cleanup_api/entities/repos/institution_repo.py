7e41d1be428ac6b835bbca0347f408fb6b4ceafe
Nargis Sultani <nargis.sultani@cfpb.gov>
2024-09-06 15:53:16 -0400

    added deleting mapping and fi history and updated poetry.lock

--- src/regtech_cleanup_api/entities/repos/institution_repo.py
+++ src/regtech_cleanup_api/entities/repos/institution_repo.py
@@ -5,6 +5,7 @@ from regtech_user_fi_management.entities.models.dao import (
     SblTypeMappingDao,
 )
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 
 
 def delete_domains_by_lei(
@@ -21,6 +22,9 @@ def delete_sbl_type_by_lei(
     session: Session, lei: str
 ) -> List[SblTypeMappingDao] | None:
     session.query(SblTypeMappingDao).filter(SblTypeMappingDao.lei == lei).delete()
+    # deleting from history tables
+    del_hist_stmt = text("DELETE from fi_to_type_mapping_history where fi_id = :fi_id")
+    session.execute(del_hist_stmt, {"fi_id": lei})
     session.commit()
     return {"OK": True}
 
@@ -29,5 +33,9 @@ def delete_institution(session: Session, lei: str) -> FinancialInstitutionDao | 
     session.query(FinancialInstitutionDao).filter(
         FinancialInstitutionDao.lei == lei
     ).delete()
+
+    # deleting from history tables
+    del_hist_stmt = text("DELETE from financial_institutions_history where lei = :lei")
+    session.execute(del_hist_stmt, {"lei": lei})
     session.commit()
     return {"OK": True}
